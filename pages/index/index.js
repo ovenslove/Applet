f67bc7f035a1d134172dc1c28aed1bf903f27183
ovenslove <1905997838@qq.com>
2016-11-14 17:16:44 +0800

    modified the index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,80 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    imgUrls: [
+      'http://mmbiz.qpic.cn/mmbiz_jpg/rFWVXwibLGty43VKrC7pO24gicT0xyLR0EvtTHLgbveCakHveEr1RpMI0em1ywiaXibvVYob98pUkn9MCtmbyxJ8Sg/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1',
+      'http://a1.qpic.cn/psb?/V14WDwZe14UYU3/UQUs.16VtVW2ZBoqvygIYx6WmgAFv32*ZLs3ddBetsc!/b/dPYAAAAAAAAA&bo=VAY4BHAXoA8FAIU!&rf=viewer_4',
+      'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg'
+    ],
+    blogList:[
+        {
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'我是标题',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        },
+        {
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'aaaaaa',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        },
+        {
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'aaaaaa',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        },
+        {
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'aaaaaa',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        },{
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'aaaaaa',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        },{
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'aaaaaa',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        },{
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'aaaaaa',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        },{
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'aaaaaa',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        },{
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'aaaaaa',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        },{
+          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
+          title:'aaaaaa',
+          content:'摩诃萨的搜嘎放虎归山大解放后'
+        }
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  changeIndicatorDots: function(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  changeAutoplay: function(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function(e) {
+    this.setData({
+      duration: e.detail.value
     })
   }
 })
