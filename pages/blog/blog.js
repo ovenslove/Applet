b76caeabe610a54a7e0bc46f46803a2b05f933fd
ovenslove <1905997838@qq.com>
2016-11-22 21:02:59 +0800

    modefied the index page

--- pages/blog/blog.js
+++ pages/blog/blog.js
@@ -1,28 +1,33 @@
 var app = getApp()
 Page({
   data: {
-
+    title:"",
+    content:""
   },
   onLoad: function (options) {
-    wx.setNavigationBarTitle({
-      title:options.title
-    });
-    this.setData({
-        title: options.title
-      });
+
       var that=this;
       //找后台要小程序允许的域名
       wx.request({
-            url: 'https://api.jqstudy.cn/blog.json',
+            url: 'https://api.jqstudy.cn/blogs/'+options.blogid,
             method: 'GET',
+            header: {
+                'content-type': 'application/json'
+            },
             success:function(res) {
               that.setData({
                   title: res.data.title,
                   content:res.data.content
                 });
-              console.log(res.data);
+
             }
 
         })
+  },
+  onReady:function () {
+    console.log();
+    wx.setNavigationBarTitle({
+      title:this.data.title
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -8,62 +8,7 @@ Page({
       'http://a1.qpic.cn/psb?/V14WDwZe14UYU3/UQUs.16VtVW2ZBoqvygIYx6WmgAFv32*ZLs3ddBetsc!/b/dPYAAAAAAAAA&bo=VAY4BHAXoA8FAIU!&rf=viewer_4',
       'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg'
     ],
-    blogList:[
-        {
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        },
-        {
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        },
-        {
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        },
-        {
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        },{
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        },{
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        },{
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        },{
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        },{
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        },{
-          blogId:'11100',
-          preImg:'http://ac-mhke0kuv.clouddn.com/9c91c094a8e20b9136c6.jpg?imageView/2/w/800/h/600/q/80/format/jpeg',
-          title:'小程序之个人博客',
-          content:'本项目是使用小程序开发模式，以一个博客APP为开发案例，尽可能涵盖小程序所有的组件和API，组装成一个完整的小程序案例'
-        }
-    ],
+    blogList:[],
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
@@ -72,5 +17,23 @@ Page({
   onPullDownRefresh:function() {
     console.log("111");
     wx.stopPullDownRefresh();
+  },
+  onLoad: function (options) {
+      var that=this;
+      //找后台要小程序允许的域名
+      wx.request({
+            url: 'https://api.jqstudy.cn/blogs',
+            method: 'GET',
+            header: {
+                'content-type': 'application/json'
+            },
+            success:function(res) {
+              that.setData({
+                  blogList: res.data
+                });
+              console.log(res.data);
+            }
+
+        })
   }
 })
